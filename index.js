3004fd2d0e11855e7ec83ccf8b858585efd14c30
Connor Knabe <connorknabe@gmail.com>
2017-11-15 17:58:17 -0600

    azure context.done added

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 var geoip = require('geoip-lite');
 
-var ip = '108.171.133.166';
-var geo = geoip.lookup(ip);
-
-// console.log(geo.region);
-// console.log(geo.city);
 module.exports = function(context, req) {
-    context.log(`calling with data ${req}`);
-    context.res = geo.city;
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    var ip = '108.171.133.166';
+    var geo = geoip.lookup(ip);
+    if (req.query.name || (req.body && req.body.name)) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: geo.city + 'Hello ' + (req.query.name || req.body.name)
+        };
+    } else {
+        context.res = {
+            status: 400,
+            body:
+                'Please pass a name on the query string or in the request body'
+        };
+    }
+    context.done();
 };
